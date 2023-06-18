071471e1690a41adb599e8d2ff8ce4ecbf2801ad
Harrison Bludworth <harrisonbludworth@gmail.com>
2023-06-17 20:15:54 -0600

    updated env variables names

--- packages/backend/src/connection.ts
+++ packages/backend/src/connection.ts
@@ -1,8 +1,8 @@
 import { MongoClient } from 'mongodb';
 
-const userName = process.env.MONGOUSER;
-const password = process.env.MONGOPASSWORD;
-const hostname = process.env.MONGOHOSTNAME;
+const userName = process.env.MONGOUSER_FB;
+const password = process.env.MONGOPASSWORD_FB;
+const hostname = process.env.MONGOHOSTNAME_FB;
 
 if (!userName || !password || !hostname) {
   throw Error('Database not configured. Set environment variables.');
